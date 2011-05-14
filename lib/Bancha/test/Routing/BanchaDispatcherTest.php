cb14479fabe63e4617a9c2317f25631c68c8a529
wokung <kung.wong@gmail.com>
2011-05-14 16:31:13 +0200

    Change-Id: Ic6ef19fe8d379e74a98b83072059c1c719f78143

---
+++ lib/Bancha/test/Routing/BanchaDispatcherTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @copyright     Copyright 2011 Bancha Project
+ * @link          http://banchaproject.org Bancha Project
+ * @since         Bancha v1.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @author        Florian Eckerstorfer <f.eckerstorfer@gmail.com>
+ */
+
+/**
+ * BanchaDispatcher
+ *
+ * @package bancha.libs
+ */
+class BanchaDispatcherTest
+{
+
+	/**
+	 * Dispatches a BanchaRequest object.
+	 *
+	 * @param BanchaRequest $requests A BanchaRequest can contain multiple CakeRequest objects.
+	 * @return boolean Success
+	 */
+	public function dispatch(BanchaRequest $requests)
+	{
+		// TODO: Actually implement BanchaDispatcher::dispatch()
+		/* This is only some demo code. The idea of this method is that it receives a BanchaRequest object which
+		   can contain multiple requests. The getRequests() method of BanchaRequest parses these multiple requests and
+		   returns a CakeRequest object for every request. Therefore this method does only need to invoke the
+		   Dispatcher (BanchaDispatcher::dispatch()) for every CakeRequest object. It is very import that the
+		   $additionalParameters array does contain the 'return' value. Then Cakes default Dispatcher does return the
+		   response instead of sending it to the client.
+		*/
+		$dispatcher = new BanchaSingleDispatcher();
+		foreach ($requests->getRequests() as $request)
+		{
+			// Call dispatcher for the given CakeRequest.
+			$response = $dispatcher->dispatch($request, array('return' => true));
+		}
+	}
+
+}
