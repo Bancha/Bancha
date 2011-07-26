Bancha/Bancha

commit 9e6159e3dc5e1d18b8822cc54812a1125845a6a8

Bancha now handles 'submit' requests from Ext JS and directs them to add/edit actions of CakePHP.

Lib/Bancha/Network/BanchaResponseCollection.php
@@ -24,7 +24,7 @@ App::uses('BanchaResponseTransformer', 'Bancha.Bancha/Network');
  * @package bancha.libs
  */
 class BanchaResponseCollection {
-	
+
 /** @var array */
 	protected $responses = array();
 
@@ -46,10 +46,10 @@ class BanchaResponseCollection {
 			'method'	=> $request->action, // actions are called methods in Ext JS
 			'result'	=> BanchaResponseTransformer::transform($response->body(), $request),
 		);
-		
+
 		return $this;
 	}
-	
+
 /**
  * Adds an exception to the BanchaResponse
  *
@@ -65,16 +65,25 @@ class BanchaResponseCollection {
 			'where'		=> 'In file "' . $e->getFile() . '" on line ' . $e->getLine() . '.',
 			'trace'		=> $e->getTraceAsString(),
 		);
-		
+
 		return $this;
 	 }
-	 
+
 /**
  * Combines all CakeResponses into a single response and transforms it into JSON.
  *
  * @return CakeResponse
  */
 	public function getResponses() {
+		if (isset($this->responses[0]) && !is_array($this->responses[0]))
+		{
+			return new CakeResponse(array(
+				'body'		=>	$this->responses,
+				'status'	=> 200,
+				'type'		=> 'text/html',
+				'charset'	=> 'utf-8',
+			));
+		}
 		return new CakeResponse(array(
 			'body'			=> json_encode($this->responses),
 			'status'		=> 200,

Lib/Bancha/Network/BanchaRequestCollection.php
@@ -26,13 +26,13 @@ App::uses('BanchaRequestTransformer', 'Bancha.Bancha/Network');
  * @package bancha.libs
  */
 class BanchaRequestCollection {
-	
+
 	/** @var string */
 	protected $rawPostData;
-	
+
 	/** @var array */
 	protected $postData;
-	
+
 	/**
 	 * Constructor.
 	 *
@@ -57,7 +57,7 @@ class BanchaRequestCollection {
 			$data = json_decode($this->rawPostData, true);
 			// TODO: improve detection (not perfect, but should it should be correct in most cases.)
 			if (isset($data['action']) || isset($data['method']) || isset($data['data'])) {
-				$data = array($data); 
+				$data = array($data);
 			}
 			$data = Set::sort($data, '{n}.tid', 'asc');
 		}
@@ -65,21 +65,22 @@ class BanchaRequestCollection {
 		{
 			$data = array($this->postData);
 		}
-		
+
 		if(count($data) > 0) {
 	 		for ($i=0; $i < count($data); $i++) {
 				$transformer = new BanchaRequestTransformer($data[$i]);
-				
+
 				$_SERVER['REQUEST_METHOD'] = 'POST';
-				
+
 				$requests[$i] = new CakeRequest($transformer->getUrl());
 				$requests[$i]['controller'] = $transformer->getController();
 				$requests[$i]['action']		= $transformer->getAction();
 				$requests[$i]['named']		= $transformer->getPaging();
 				$requests[$i]['pass']		= $transformer->getPassParams();
 				$requests[$i]['tid']		= $transformer->getTid();
-				$requests[$i]['plugin']	= null;
-				
+				$requests[$i]['plugin']		= null;
+				$requests[$i]['extUpload']	= $transformer->getExtUpload();
+
 				foreach ($transformer->getCleanedDataArray() as $key => $value) {
 					$requests[$i]->data($key, $value);
 				}

Lib/Bancha/Network/BanchaRequestTransformer.php
@@ -25,27 +25,30 @@ App::uses('ArrayConverter', 'Bancha.Bancha/Utility');
  * @package bancha.libs
  */
 class BanchaRequestTransformer {
-	
+
 /** @var array */
 	private $data;
-	
+
 /** @var string */
 	protected $controller = null;
-	
+
 /** @var string */
 	protected $action = null;
-	
+
 /** @var string */
 	protected $url = null;
-	
+
 /** @var array */
 	protected $paginate = array();
-	
+
 /** @var boolean TRUE if the given request is a form request. */
 	protected $isFormRequest = false;
-	
-	/** @var boolean */
+
+/** @var boolean */
 	protected $tid;
+
+/** @var boolean TRUE if the given request is an upload request. */
+	protected $extUpload;
 
 /**
  * Constructor. Requires a single Ext JS request in PHP array format.
@@ -56,7 +59,7 @@ class BanchaRequestTransformer {
 	{
 		$this->data = $data;
 	}
-	
+
 /**
  * Returns the name of the controller. Thus returns the value of 'action' from the Ext JS request. Also removes the
  * 'action' property from the Ext JS request.
@@ -82,7 +85,7 @@ class BanchaRequestTransformer {
 		}
 		return $this->controller;
 	}
-	
+
 /**
  * Returns the name of the action. Thus returns the value of 'method' from the Ext JS request. Because Ext JS and
  * CakePHP use different names for CRUD operations, this method also transforms it according to the following list:
@@ -114,6 +117,9 @@ class BanchaRequestTransformer {
 		}
 		switch ($this->action)
 		{
+			case 'submit':
+				$this->action = (isset($this->data['data']['id']) || isset($this->data['id'])) ? 'edit' : 'add';
+				break;
 			case 'create':
 				$this->action = 'add';
 				break;
@@ -129,7 +135,17 @@ class BanchaRequestTransformer {
 		}
 		return $this->action;
 	}
-	
+
+	public function getExtUpload()
+	{
+		if (null != $this->extUpload)
+		{
+			return $this->extUpload;
+		}
+		$this->extUpload = isset($this->data['extUpload']) ? $this->data['extUpload'] : false;
+		return $this->extUpload;
+	}
+
 /**
  * If an URL is provided in the Ext JS request, this method returns it and removes it from the Ext JS request.
  *
@@ -144,7 +160,7 @@ class BanchaRequestTransformer {
 		}
 		return $this->url;
 	}
-	
+
 	public function getTid()
 	{
 		if (null != $this->tid)
@@ -163,7 +179,7 @@ class BanchaRequestTransformer {
 		}
 		return $this->tid;
 	}
-	
+
 /**
  * Returns the 'pass' parameters from the Ext JS request. 'pass' parameters are special parameters which are passed
  * directly to the controller/action by CakePHP. The only 'pass' parameter that exist for the CRUD operations is 'id'
@@ -177,7 +193,7 @@ class BanchaRequestTransformer {
 		{
 			return array();
 		}
-		
+
 		$pass = array();
 		if (isset($this->data['data']['id']))
 		{
@@ -192,7 +208,7 @@ class BanchaRequestTransformer {
 		}
 		return $pass;
 	}
-	
+
 /**
  * Returns the paging options in a format suited for CakePHP. If a page number is provided by the Ext JS request, it
  * returns this page number directly, otherwise, if provided, it calculates the page number from the start offset and
@@ -208,7 +224,7 @@ class BanchaRequestTransformer {
 		{
 			return $this->paginate;
 		}
-		
+
 		$page = 1;
 		if (isset($this->data['data']['page']))
 		{
@@ -245,9 +261,9 @@ class BanchaRequestTransformer {
 		);
 		return $this->paginate;
 	}
-	
+
 /**
- * Returns the data array from the Ext JS request without all special elements. Therefore it calls all the get*() 
+ * Returns the data array from the Ext JS request without all special elements. Therefore it calls all the get*()
  * methods in the class, which not only return the values but also clean the request.
  *
  * @return array Cleaned data array.

Lib/Bancha/Network/BanchaResponseTransformer.php
@@ -3,15 +3,15 @@
 class BanchaResponseTransformer
 {
 
-	public static function transform($response, CakeRequest $request) {	
+	public static function transform($response, CakeRequest $request) {
 		$data = array();
 		$modelName = null;
-		
+
 		if ($request->controller)
 		{
 			$modelName = Inflector::camelize(Inflector::singularize($request->controller));
 		}
-		
+
 		if ('index' == $request->action && $modelName)
 		{
 			foreach ($response as $i => $element) {
@@ -27,6 +27,12 @@ class BanchaResponseTransformer
 		{
 			$response = $response[$modelName];
 		}
+
+		if (isset($request['extUpload']) && $request['extUpload'])
+		{
+			return '<html><body><textarea>' . json_encode($response) . '</textarea></body></html>';
+		}
+
 		return $response;
 	}
 

Test/Case/AllSystemTest.php
@@ -38,10 +38,11 @@ class AllSystemTest extends PHPUnit_Framework_TestSuite {
 
 		$path = dirname(__FILE__) . DS . 'System';
 		$suite->addTestFile($path . DS . 'BanchaCrudTest.php');
+		$suite->addTestFile($path . DS . 'BanchaExceptionsTest.php');
 		$suite->addTestFile($path . DS . 'BanchaFormTest.php');
 		$suite->addTestFile($path . DS . 'ConsistentModelTest.php');
 
-		$suite->addTestDirectory($path);
+		// $suite->addTestDirectory($path);
 		return $suite;
 	}
 }

Test/Case/AllTestsTest.php
@@ -42,7 +42,7 @@ class AllTestsTest extends PHPUnit_Framework_TestSuite {
 		$suite->addTestFile($path . 'AllNetworkTest.php');
 		$suite->addTestFile($path . 'AllUtilityTest.php');
 
-		$suite->addTestDirectory($path);
+		// $suite->addTestDirectory($path);
 		return $suite;
 	}
 }

Test/Case/Network/BanchaRequestCollectionTest.php
@@ -24,7 +24,7 @@ App::uses('BanchaRequestCollection', 'Bancha.Bancha/Network');
  */
 class BanchaRequestCollectionTest extends CakeTestCase
 {
-	
+
 /**
  * Transforms one Ext JS request into a CakePHP request. Transforms the indexes from Ext JS syntax (action + method)
  * into CakePHP syntax (controller + action).
@@ -44,23 +44,23 @@ class BanchaRequestCollectionTest extends CakeTestCase
 		// Create a new request collection and parse the requests by calling getRequests().
 		$collection = new BanchaRequestCollection($rawPostData);
 		$requests = $collection->getRequests();
-		
+
 		// This should generate 1 CakeRequest object packed in an array.
 		$this->assertEquals(1, count($requests));
 		$this->assertThat($requests[0], $this->isInstanceOf('CakeRequest'));
-		
+
 		// All requests should be POST requests
 		$this->assertTrue($requests[0]->is('post'));
-		
+
 		// action -> controller
 		$this->assertEquals($requests[0]['controller'], 'Test');
 		// method -> actio AND "create" -> "add"
 		$this->assertEquals($requests[0]['action'], 'add');
-		
+
 		// Cake has some special params like paginate, pass and named. Assure that these are there.
 		$this->assertTrue(isset($requests[0]['pass']));
 		$this->assertTrue(isset($requests[0]['named']));
-		
+
 		// TID is set?
 		$this->assertEquals(1, $requests[0]['tid']);
 	}
@@ -92,16 +92,16 @@ class BanchaRequestCollectionTest extends CakeTestCase
 
 		$collection = new BanchaRequestCollection($rawPostData);
 		$requests = $collection->getRequests();
-		
+
 		// Two requests should result in an array that contains 2 CakeRequest objects.
 		$this->assertEquals(2, count($requests));
 		$this->assertThat($requests[0], $this->isInstanceOf('CakeRequest'));
 		$this->assertThat($requests[1], $this->isInstanceOf('CakeRequest'));
-		
+
 		// All requests should be POST requests
 		$this->assertTrue($requests[0]->is('post'));
 		$this->assertTrue($requests[1]->is('post'));
-		
+
 		// action -> controller
 		$this->assertEquals($requests[0]['controller'], 'Test');
 		$this->assertEquals($requests[1]['controller'], 'Test');
@@ -109,9 +109,9 @@ class BanchaRequestCollectionTest extends CakeTestCase
 		$this->assertEquals($requests[0]['action'], 'add');
 		$this->assertEquals($requests[1]['action'], 'edit');
 	}
-	
+
 /**
- * Transforms one Ext JS form request into a CakePHP request. Transforms the indexes from Ext JS form syntax (action + 
+ * Transforms one Ext JS form request into a CakePHP request. Transforms the indexes from Ext JS form syntax (action +
  * method) into CakePHP syntax (controller + action).
  *
  */
@@ -143,15 +143,33 @@ class BanchaRequestCollectionTest extends CakeTestCase
 		// Cake has some special params like paginate, pass and named. Assure that these are there.
 		$this->assertTrue(isset($requests[0]['pass']));
 		$this->assertTrue(isset($requests[0]['named']));
-		
+
 		// ID needs to be added to the 'pass' array.
 		$this->assertEquals(42, $requests[0]['pass']['id']);
-		
+
 		// Title needs to be added to the data array.
 		$this->assertEquals('Hello World', $requests[0]->data('title'));
-		
+
 		// TID is set?
 		$this->assertEquals(1, $requests[0]['tid']);
+	}
+
+	public function testGetExtUploadForm()
+	{
+		$postData = array(
+			'extAction'	=> 'Test',
+			'extMethod'	=> 'update',
+			'extTID'	=> 1,
+			'id'		=> 42,
+			'title'		=> 'Hello World',
+			'extUpload'	=> true,
+		);
+
+		// Create a new request collection and parse the requests by calling getRequests().
+		$collection = new BanchaRequestCollection('', $postData);
+		$requests = $collection->getRequests();
+
+		$this->assertEquals(true, $requests[0]['extUpload']);
 	}
 
 }

Test/Case/Network/BanchaRequestTransformerTest.php
@@ -23,7 +23,7 @@ App::uses('BanchaRequestTransformer', 'Bancha.Bancha/Network');
  */
 class BanchaRequestTransformerTest extends CakeTestCase
 {
-	
+
 /**
  * In the Ext JS request the name of the controller is stored as "action". We need to transform this.
  *
@@ -36,7 +36,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertNotNull($transformer->getController());
 		$this->assertEquals('Test', $transformer->getController());
 	}
-	
+
 	public function testGetControllerForm()
 	{
 		$transformer = new BanchaRequestTransformer(array(
@@ -45,7 +45,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertNotNull($transformer->getController());
 		$this->assertEquals('Test', $transformer->getController());
 	}
-	
+
 /**
  * First the name of action is stored in the "method" property in the Ext JS request, second Ext JS use different names
  * for CRUD operations. We need to transform them.
@@ -67,7 +67,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertNotNull($transformer->getAction());
 		$this->assertEquals($cakeAction, $transformer->getAction());
 	}
-	
+
 /**
  * @dataProvider getActionProvider
  */
@@ -80,7 +80,16 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertNotNull($transformer->getAction());
 		$this->assertEquals($cakeAction, $transformer->getAction());
 	}
-	
+
+	public function testGetExtUpload()
+	{
+		$transformer = new BanchaRequestTransformer(array(
+			'extUpload'		=> true,
+		));
+		$this->assertNotNull($transformer->getExtUpload());
+		$this->assertEquals(true, $transformer->getExtUpload());
+	}
+
 /**
  * If the Ext JS request contains an URL, we need to extract is from the request, because we need to pass it to the
  * Constructor of CakeRequest.
@@ -94,7 +103,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertNotNull($transformer->getUrl());
 		$this->assertEquals('/test/action', $transformer->getUrl());
 	}
-	
+
 /**
  * There are some params which are passed directly to the action method inside the controller. CakePHP stores them in
  * the 'pass' array inside the request. For the CRUD operations the only 'pass' parameter is 'id'. Therefore we extract
@@ -109,7 +118,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		));
 		$this->assertEquals(array('id' => 42), $transformer->getPassParams());
 	}
-	
+
 	public function testGetPassParamsForm()
 	{
 		$transformer = new BanchaRequestTransformer(array(
@@ -118,13 +127,13 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		));
 		$this->assertEquals(array('id' => 42), $transformer->getPassParams());
 	}
-	
+
 /**
- * Ext JS uses page, offset, limit and sort in the data array for pagination. CakePHP needs a paging array with 
+ * Ext JS uses page, offset, limit and sort in the data array for pagination. CakePHP needs a paging array with
  * page, limit and order. The sort in Ext looks like [property: X, direction: Y], in Cake like [Controller.X => Y].
  *
  * @dataProvider getPagingProvider
- */	
+ */
 	public function testGetPaging($extData, $cakePaginate)
 	{
 		$data = array(
@@ -139,7 +148,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertEquals($paging['limit'], $cakePaginate['limit']);
 		$this->assertEquals($paging['order'], $cakePaginate['order']);
 	}
-	
+
 	public function testGetTid()
 	{
 		$data = array(
@@ -149,7 +158,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$transformer = new BanchaRequestTransformer($data);
 		$this->assertEquals(42, $transformer->getTid());
 	}
-	
+
 	public function testGetTidForm()
 	{
 		$data = array(
@@ -159,7 +168,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$transformer = new BanchaRequestTransformer($data);
 		$this->assertEquals(42, $transformer->getTid());
 	}
-	
+
 /**
  * The data array (which represent POST parameters) in CakePHP only contains the actual data values but not the special
  * parameters. Thus we need to clean the data array from action, controller, paginate and pass parameters. We therefore
@@ -193,7 +202,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertFalse(isset($data['tid']));
 		$this->assertEquals('bar', $data['foo']);
 	}
-	
+
 	public function testGetCleanedDataArrayForm()
 	{
 		$data = array(
@@ -212,7 +221,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 		$this->assertEquals('bar', $data['foo']);
 		$this->assertFalse(isset($data['extTID']));
 	}
-	
+
 /**
  * Provides the action names from Ext JS and CakePHP for use in testGetAction().
  *
@@ -227,7 +236,7 @@ class BanchaRequestTransformerTest extends CakeTestCase
 			array('read', array(), 'index'),
 		);
 	}
-	
+
 /**
  * Data provider for testGetRequestsPagination().
  *
@@ -277,5 +286,5 @@ class BanchaRequestTransformerTest extends CakeTestCase
 			),
 		);
 	}
-	
+
 }

commit 425e3a08420f95155721f0f5064a7c64732d0d41

Fixed a bug with extUpload requests introduced in 9e6159e3dc5e1d18b882.

Lib/Bancha/Network/BanchaResponseCollection.php
@@ -41,13 +41,18 @@ class BanchaResponseCollection {
  * @return BanchaResponseCollection
  */
 	public function addResponse($tid, CakeResponse $response, CakeRequest $request, $exception = false) {
-		$this->responses[] = array(
+		$response = array(
 			'type'		=> 'rpc',
 			'tid'		=> $tid,
 			'action'	=> $request->controller, // controllers are called action in Ext JS
 			'method'	=> $request->action, // actions are called methods in Ext JS
 			'result'	=> BanchaResponseTransformer::transform($response->body(), $request),
 		);
+		if ($request['extUpload'])
+		{
+			$response['extUpload'] = true;
+		}
+		$this->responses[] = $response;
 
 		return $this;
 	}
@@ -79,9 +84,9 @@ class BanchaResponseCollection {
  */
 	public function getResponses() {
 		// Response to ExtUpload request
-		if (isset($this->responses[0]) && !is_array($this->responses[0])) {
+		if (isset($this->responses[0]['extUpload']) && $this->responses[0]['extUpload']) {
 			return new CakeResponse(array(
-				'body'		=>	$this->responses,
+				'body'		=>	$this->responses[0]['result'],
 				'status'	=> 200,
 				'type'		=> 'text/html',
 				'charset'	=> 'utf-8',

Test/Case/Network/BanchaResponseCollectionTest.php
@@ -40,28 +40,49 @@ class BanchaResponseCollectionTest extends CakeTestCase
 		$request2->addParams(array('controller' => 'bar', 'action' => 'foo'));
 		$request3 = new CakeRequest();
 		$request3->addParams(array('controller' => 'foo', 'action' => 'error'));
-		
+
 		$collection = new BanchaResponseCollection();
 		$collection->addResponse(1, new CakeResponse($response1), $request1)
 				   ->addResponse(2, new CakeResponse($response2), $request2)
 				   ->addException(3, $response3, $request3);
-		
+
 		$actualResponse = json_decode($collection->getResponses()->body());
-		
+
 		$this->assertEquals('rpc', $actualResponse[0]->type);
 		$this->assertEquals(1, $actualResponse[0]->tid);
 		$this->assertEquals('foo', $actualResponse[0]->action);
 		$this->assertEquals('bar', $actualResponse[0]->method);
 		$this->assertEquals((object)array('message' => 'Hello World'), $actualResponse[0]->result);
-		
+
 		$this->assertEquals('rpc', $actualResponse[1]->type);
 		$this->assertEquals(2, $actualResponse[1]->tid);
 		$this->assertEquals('bar', $actualResponse[1]->action);
 		$this->assertEquals('foo', $actualResponse[1]->method);
 		$this->assertEquals((object)array('message' => 'Hello Bancha'), $actualResponse[1]->result);
-		
+
 		$this->assertEquals('exception', $actualResponse[2]->type);
 		$this->assertEquals('This is an exception', $actualResponse[2]->message);
 		$this->assertEquals('In file "' . __FILE__ . '" on line ' . $exception_line . '.', $actualResponse[2]->where);
 	}
+
+/**
+ * Tests the getResponses() method in combination with the 'extUpload' request parameter. If this parameter is true,
+ * the response should not be JSON encoded but rather a valid HTML structure which contains the result inside a
+ * <textarea>-element.
+ *
+ */
+	public function testGetResponses_extUpload() {
+		$response1 = array(
+			'body'	=> array('message' => 'Hello World'),
+		);
+		$request = new CakeRequest();
+		$request->addParams(array('extUpload' => 1));
+
+		$collection = new BanchaResponseCollection();
+		$collection->addResponse(1, new CakeResponse($response1), $request);
+
+		$this->assertEquals('<html><body><textarea>' . json_encode($response1['body']) . '</textarea></body></html>',
+				$collection->getResponses()->body());
+	}
+
 }