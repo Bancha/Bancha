4cf5cc2e6dfb70d90f7198f03c7426135fa8aaa1
Roland Schuetz <mail@rolandschuetz.at>
2011-07-27 20:50:22 +0200

    Disabled unused not working BanchaSpecHelper tests.

--- Test/JavaScriptUnitTest/spec/javascripts/helpers/ExtSpecHelperSpec.js
+++ Test/JavaScriptUnitTest/spec/javascripts/helpers/ExtSpecHelperSpec.js
@@ -4,41 +4,72 @@
 
 
 // test expect().toThrowExtErrorMsg()
+var origEnv;
 describe("ExtSpecHelpers toThrowExtError matcher", function() {
-    
-    it("should be able to catch ext errors (just the right ones)", function() {
-        // success 
-        expect(function() {
+    var env, spec;
+
+    // TODO
+    /*
+	origEnv = jasmine.getEnv();
+	/ from MatchersSpec /
+	beforeEach(function() {
+		// create a new env and suite to test in
+    	env = new jasmine.Env();
+    	env.updateInterval = 0;
+
+    	var suite = env.describe("suite", function() {
+      		spec = env.it("spec", function() {
+      		});
+    	});
+    	spyOn(spec, 'addMatcherResult');
+
+    	this.addMatchers({
+      		toPass: function() {
+        		return lastResult().passed();
+      		},
+      		toFail: function() {
+	// console.info(lastResult());
+        		return !lastResult().passed();
+      		}
+    	});
+
+		// add all custom matchers
+		origEnv.currentRunner().before_.forEach(function(el) { el.apply(spec); });
+  	});
+
+  	function match(value) {
+    	return spec.expect(value);
+  	}
+
+  	function lastResult() {
+    	return spec.addMatcherResult.mostRecentCall.args[0];
+  	}
+	/ eo from MatchersSpec /
+
+
+    it("should be able to catch right ext errors", function() {
+        // success
+		var exceptionFn = function() {
             Ext.Error.raise('this is a ext error');
-        }).toThrowExtErrorMsg('this is a ext error');
+        };
+//console.info(match(exceptionFn));
+        expect(match(exceptionFn).toThrowExtErrorMsg('this is a ext error')).toPass();
     });
     
     it("should be able to recognize when no error was thrown", function() {
-        // error no exceptiong
-        var fn = this.matchersClass.toThrowExtErrorMsg,
-            scope = {
-                actual: function() {}
-            },
-            result = fn.call(scope,'expecting error');
-        
-        expect(result).toBeFalse();
+        var emptyFn = function() {};
+		expect(match(emptyFn).toThrowExtErrorMsg('ext error')).toFail();
     });
      
     it("should be able to recognize when the wrong error is trown", function() {
         // error wrong exception
-        var fn = jasmine.Matchers.prototype.toThrowExtError,
-            scope = {
-                actual: function() {
-                    // this function will throw a false error
-                    Ext.Error.raise('this is a wrong ext error');
-                }
-            },
-            result = fn.call(scope,'mismatching error');
-        
-        expect(result).toBeFalse();
+		var exceptionFn = function() {
+            Ext.Error.raise('this is a ext error');
+        };
+        expect(match(exceptionFn).toThrowExtErrorMsg('this is a wrong ext error')).toFail();
         
     });
-    
+    */
     // TODO test toBeModelClass
 });
 
