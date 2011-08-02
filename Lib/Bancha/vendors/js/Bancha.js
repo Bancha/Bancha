6595efc85c8bc54e91005e5fcd01dab79d42a111
Roland Schuetz <mail@rolandschuetz.at>
2011-08-02 04:09:10 +0200

    Bug Fix

--- Lib/Bancha/vendors/js/Bancha.js
+++ Lib/Bancha/vendors/js/Bancha.js
@@ -162,14 +162,18 @@ Ext.define('Bancha.data.writer.ConsistentJson', { // TODO testen
         fileMessage: "is not a valid file",
         /**
          * @method
-         * Validates the value is a number in the range of minValue and maxValue.
-         * Precision is not validated, it's just meta data for scaffolding.
+         * Validates that the number is in the range of min and max.
+         * Precision is not validated, but it is used for differenting int from float,
+         * also it's metadata for scaffolding.
          * For example:
          *     {type: 'numberformat', field: 'euro', precision:2, min:0, max: 1000}
          */
         numberformat: function(config, value) {
             if(typeof value !== 'number') {
-                return false; // not a number
+                value = (config.precision===0) ? parseInt(value,10) : parseFloat(value);
+                if(typeof value !== 'number') {
+                    return false; // could not be converted to a number
+                }
             }
             if((Ext.isDefined(config.min) && config.min > value) || (Ext.isDefined(config.max) && value > config.max)) {
                 return false; // not in the range
