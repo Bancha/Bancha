5fb5762b2976696a432ba15be0ab167c13c2375c
Roland Schuetz <mail@rolandschuetz.at>
2012-01-23 17:47:22 +0100

    Cleaned up tests, now the use fixtures

---
+++ Test/Case/Controller/BanchaControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * BanchaControllerTest file.
+ *
+ * Bancha Project : Combining Ext JS and CakePHP (http://banchaproject.org)
+ * Copyright 2011, Roland Schuetz, Kung Wong, Andreas Kern, Florian Eckerstorfer
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2011 Roland Schuetz, Kung Wong, Andreas Kern, Florian Eckerstorfer
+ * @link          http://banchaproject.org Bancha Project
+ * @since         Bancha v1.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @author        Florian Eckerstorfer <f.eckerstorfer@gmail.com>
+ * @author        Roland Schuetz <mail@rolandschuetz.at>
+ */
+
+/**
+ * BanchaControllerTest
+ * @package       Bancha
+ * @category      tests
+ */
+class BanchaControllerTest extends ControllerTestCase {
+	public $fixtures = array('plugin.bancha.article','plugin.bancha.user','plugin.bancha.tag','plugin.bancha.articles_tag');
+
+
+	// TODO add tests here, see http://book.cakephp.org/2.0/en/development/testing.html#testing-controllers
+	
+	
+}
+
+    
