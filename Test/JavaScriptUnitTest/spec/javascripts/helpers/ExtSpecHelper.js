6f84e37717b8fcee33894476ad6fa745e6ef81c5
Roland Schütz <mail@rolandschuetz.at>
2012-06-11 22:06:06 +0200

    Code cleanup

--- Test/JavaScriptUnitTest/spec/javascripts/helpers/ExtSpecHelper.js
+++ Test/JavaScriptUnitTest/spec/javascripts/helpers/ExtSpecHelper.js
@@ -19,46 +19,46 @@ beforeEach(function() {
     
     
     
-	/**
-	 * used for matcher isModelClass
-	 * Safely finds an object, used internally for getStubsNamespace and getRemoteApi
-	 * (This function is tested in RS.util, not part of the package testing, but it is tested)
-	 * @param {String} path A period ('.') separated path to the desired object (String).
-	 * @param {String} lookIn optional: The object on which to perform the lookup.
-	 * @return {Object} The object if found, otherwise undefined.
-	 * @member Bancha
-	 * @method objectFromPath
-	 * @private
-	 */
-	var objectFromPath = function(path, lookIn) {
-	    if (!lookIn) {
-	        //get the global object so it don't use hasOwnProperty on window (IE incompatible)
-	        var first = path.indexOf('.'),
-	            globalObjName,
-	            globalObj;
-	        if (first === -1) {
-	            // the whole path is only one object so eturn the result
-	            return window[path];
-	        }
-	        // else the first part as global object name
-	        globalObjName = path.slice(0, first);
-	        globalObj = window[globalObjName];
-	        if (typeof globalObj === 'undefined') {
-	            // path seems to be false
-	            return undefined;
-	        }
-	        // set the ne lookIn and the path
-	        lookIn = globalObj;
-	        path = path.slice(first + 1);
-	    }
-	    // get the object
-	    return path.split('.').reduce(function(o, p) {
-	        if(o && o.hasOwnProperty(p)) {
-	            return o[p];
-	        }
-	    }, lookIn);
-	};
-	
+    /**
+     * used for matcher isModelClass
+     * Safely finds an object, used internally for getStubsNamespace and getRemoteApi
+     * (This function is tested in RS.util, not part of the package testing, but it is tested)
+     * @param {String} path A period ('.') separated path to the desired object (String).
+     * @param {String} lookIn optional: The object on which to perform the lookup.
+     * @return {Object} The object if found, otherwise undefined.
+     * @member Bancha
+     * @method objectFromPath
+     * @private
+     */
+    var objectFromPath = function(path, lookIn) {
+        if (!lookIn) {
+            //get the global object so it don't use hasOwnProperty on window (IE incompatible)
+            var first = path.indexOf('.'),
+                globalObjName,
+                globalObj;
+            if (first === -1) {
+                // the whole path is only one object so eturn the result
+                return window[path];
+            }
+            // else the first part as global object name
+            globalObjName = path.slice(0, first);
+            globalObj = window[globalObjName];
+            if (typeof globalObj === 'undefined') {
+                // path seems to be false
+                return undefined;
+            }
+            // set the ne lookIn and the path
+            lookIn = globalObj;
+            path = path.slice(first + 1);
+        }
+        // get the object
+        return path.split('.').reduce(function(o, p) {
+            if(o && o.hasOwnProperty(p)) {
+                return o[p];
+            }
+        }, lookIn);
+    };
+    
     this.addMatchers({
         // now add a custom matcher to test methods where ext errors get thrown
         toThrowExtErrorMsg: function(msg) {
