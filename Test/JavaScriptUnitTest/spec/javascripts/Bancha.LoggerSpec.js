115332945d565d1d630b1d90e98bdcd4e51e2f05
Roland Schuetz <mail@rolandschuetz.at>
2013-06-14 09:53:18 +0200

    Copyright update

--- Test/JavaScriptUnitTest/spec/javascripts/Bancha.LoggerSpec.js
+++ Test/JavaScriptUnitTest/spec/javascripts/Bancha.LoggerSpec.js
@@ -1,11 +1,11 @@
 /*!
  *
  * Bancha Project : Seamlessly integrates CakePHP with ExtJS and Sencha Touch (http://banchaproject.org)
- * Copyright 2011-2012 StudioQ OG
+ * Copyright 2011-2013 StudioQ OG
  *
  * Tests for the main Bancha class
  *
- * @copyright     Copyright 2011-2012 StudioQ OG
+ * @copyright     Copyright 2011-2013 StudioQ OG
  * @link          http://banchaproject.org Bancha Project
  * @author        Roland Schuetz <mail@rolandschuetz.at>
  * @version       Bancha v PRECOMPILER_ADD_RELEASE_VERSION
