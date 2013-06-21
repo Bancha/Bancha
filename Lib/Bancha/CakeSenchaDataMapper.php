471f4bd053ad94f9a29eb227f19a673c06c11ffe
Roland Schuetz <mail@rolandschuetz.at>
2013-06-21 14:05:12 +0200

    Added phpdocs

--- Lib/Bancha/CakeSenchaDataMapper.php
+++ Lib/Bancha/CakeSenchaDataMapper.php
@@ -34,18 +34,41 @@ class CakeSenchaDataMapper {
 		$this->primary = $primaryModelName;
 	}
 
+	/**
+	 * Returns true if the current set is in cakes find('first') data structure
+	 * @return boolean True if this is a single record array
+	 */
 	public function isSingleRecord() {
 		return isset($this->data[$this->primary]);
 	}
 
+	/**
+	 * Returns true if the current set is in cakes find('all') data structure
+	 * @return boolean True if this is a record array for possibly multiple records
+	 */
 	public function isRecordSet() {
 		return isset($this->data['0']) && isset($this->data['0'][$this->primary]) && is_array($this->data['0'][$this->primary]);
 	}
 
+	/**
+	 * Returns true if the current set is in cakes find('threaded') data structure
+	 * @return boolean True if this is a record array is threaded
+	 */
 	public function isThreadedRecordSet() {
 		return $this->isRecordSet() && isset($this->data[0]['children']);
 	}
 
+	/**
+	 * Returns true if the current set is in Bancha's pagination data structure
+	 *
+	 * Example: 
+	 *     array(
+	 *         'count' => 100,
+	 *         'records' => array( ... find('all') structure... )
+	 *     )
+	 * 
+	 * @return boolean True if this is a pagination set
+	 */
 	public function isPaginatedSet() {
 		return isset($this->data['records']) && isset($this->data['count']) &&  // this is how a paginated result set should look with Bancha
 				(isset($this->data['records']['0'][$this->primary]) || 			// paginagted records with records
