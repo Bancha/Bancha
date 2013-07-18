e3540ebaa3e5bc5d2f7800351e9b5d37c6334ab9
Roland Schuetz <mail@rolandschuetz.at>
2013-07-18 16:14:18 +0200

    Added Bancha version, updated copyright

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -17,6 +17,9 @@
 // don't make "Banchas", keep it Bancha
 Inflector::rules('plural', array('/^Bancha$/i' => 'Bancha'));
 
+// Bancha version
+Configure::write('Bancha.version','PRECOMPILER_ADD_RELEASE_VERSION');
+
 /**
  * If this config is set to true all exceptions which are send via Bancha
  * are written to the CakePHP error logs.
