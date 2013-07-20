d55e5a8e3efcfa16dfae401372af4d088525962d
Roland Schuetz <mail@rolandschuetz.at>
2013-07-20 17:48:45 +0300

    PHPDocs

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -17,39 +17,71 @@
 // don't make "Banchas", keep it Bancha
 Inflector::rules('plural', array('/^Bancha$/i' => 'Bancha'));
 
-// Bancha version
-Configure::write('Bancha.version','PRECOMPILER_ADD_RELEASE_VERSION');
+/**
+ * This is the current Bancha release version.
+ */
+Configure::write('Bancha.version', 'PRECOMPILER_ADD_RELEASE_VERSION');
+
+/**
+ * If you want to use exposed controller methods to a different namespace
+ * then 'Bancha.RemoteStubs', you can override this in your core.php.  
+ * 
+ * Normally there is no reason to do this.
+ */
+Configure::write('Bancha.Api.stubsNamespace', 'Bancha.RemoteStubs');
+
+/**
+ * This is the namespace for the Ext.Direct Remote API.  
+ * 
+ * Default is 'Bancha_REMOTE_API', you can override this in your core.php.
+ * If you change this property you also need to change the JavaScript  
+ * 'Bancha.remoteApi' property. There should be no reason to do this!  
+ *
+ * If you feel like changing this, please write us an email before to
+ * support@banchaproject.org, you're probably doing something wrong.
+ */
+Configure::write('Bancha.Api.remoteApiNamespace', 'Bancha.REMOTE_API');
+
+/**
+ * There is no known reason to every enable this, please think twice before 
+ * enabling it, since it maybe makes it very hard for you to find errors.  
+ * 
+ * If you feel like changing this, please write us an email before to
+ * support@banchaproject.org, you're probably doing something wrong.  
+ * 
+ * If you want to send multiple multiple records from ExtJS to CakePHP in one 
+ * action (this is not about request batching!), you have to enable this. 
+ * Normally this is not needed and the according error is only triggered 
+ * because the ExtJS store proxy is configured with batchActions:true.  
+ * 
+ * Please never batch records on the proxy level (Ext.Direct is batching them). 
+ */
+Configure::write('Bancha.allowMultiRecordRequests', alse);
 
 /**
  * If this config is set to true all exceptions which are send via Bancha
- * are written to the CakePHP error logs.
+ * are written to the CakePHP error logs.  
  * 
  * To change disable it, please override it in your core.php
  */
-Configure::write('Bancha.logExceptions',true);
+Configure::write('Bancha.logExceptions', true);
 
 /**
  * To find bugs more easily and fix them fast, if this feature is activated,
  * Bancha provides exceptions to the Bancha developers, including environment 
- * informations like the PHP and CakePHP version, but without any data.
+ * informations like the PHP and CakePHP version, but without any data.  
  * 
  * To disable it, please override it in your core.php
  */
-Configure::write('Bancha.ServerLogger.logIssues',true);
+Configure::write('Bancha.ServerLogger.logIssues', true);
 
 /**
  * To get a better idea what server environments are the most important
  * to test and when features we should implement next, if this feature
- * is activated, Bancha will provide usage information, including environment informations
- * informations like the PHP and CakePHP version, but without any data.
+ * is activated, Bancha will provide usage information, including environment 
+ * informations like the PHP and CakePHP version, but without any data.  
  * 
  * To disable it, please override it in your core.php
  */
 Configure::write('Bancha.ServerLogger.logEnvironment', true);
 
-// config defaults
-Configure::write('Bancha.Api.AuthConfig',false);
-Configure::write('Bancha.Api.stubsNamespace','Bancha.RemoteStubs');
-Configure::write('Bancha.Api.remoteApiNamespace','Bancha.REMOTE_API');
-
-Configure::write('Bancha.allowMultiRecordRequests',false);
