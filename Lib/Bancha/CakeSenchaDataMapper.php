7e213f97f74b3552fa1879055265a0de02d6f267
Roland Schuetz <mail@rolandschuetz.at>
2013-09-17 03:19:55 +0700

    CakeSenchaDataMapper now follows the CakePHP method and property visability coding standards and has a stricter signature

--- Lib/Bancha/CakeSenchaDataMapper.php
+++ Lib/Bancha/CakeSenchaDataMapper.php
@@ -20,99 +20,117 @@
  * @since         Bancha v 2.0.0
  */
 class CakeSenchaDataMapper {
-	private $data;
-	private $primary;
 
-	/**
-	 * Build a new mapper.
-	 * @param array  $data             The CakePHP data in any format (single, multiple, threaded, paginated)
-	 * @param string $primaryModelName The name of the primary cake model for this dataset
-	 */
-	function __construct($data, $primaryModelName) {
-		$this->data = $data;
-		$this->primary = $primaryModelName;
+/** 
+ * The CakePHP data in any format (single, multiple, threaded, paginated)
+ * @var array
+ */
+	protected $_data;
+
+/**
+ * The name of the primary cake model for this dataset
+ * @var string
+ */
+	protected $_primary;
+
+/**
+ * Constructor
+ * 
+ * @param array  $data             The CakePHP data in any format (single, multiple, threaded, paginated)
+ * @param string $primaryModelName The name of the primary cake model for this dataset
+ */
+	public function __construct(array $data, $primaryModelName) {
+		$this->_data = $data;
+		$this->_primary = $primaryModelName;
 	}
 
-	/**
-	 * Returns true if the current set is in cakes find('first') data structure
-	 * @return boolean True if this is a single record array
-	 */
+/**
+ * Returns true if the current set is in cakes find('first') data structure
+ * @return boolean True if this is a single record array
+ */
 	public function isSingleRecord() {
-		return isset($this->data[$this->primary]);
+		return isset($this->_data[$this->_primary]);
 	}
 
-	/**
-	 * Returns true if the current set is in cakes find('all') data structure
-	 * @return boolean True if this is a record array for possibly multiple records
-	 */
+/**
+ * Returns true if the current set is in cakes find('all') data structure
+ * @return boolean True if this is a record array for possibly multiple records
+ */
 	public function isRecordSet() {
-		return isset($this->data['0']) && isset($this->data['0'][$this->primary]) && is_array($this->data['0'][$this->primary]);
+		return isset($this->_data['0']) && isset($this->_data['0'][$this->_primary]) && is_array($this->_data['0'][$this->_primary]);
 	}
 
-	/**
-	 * Returns true if the current set is in cakes find('threaded') data structure
-	 * @return boolean True if this is a record array is threaded
-	 */
+/**
+ * Returns true if the current set is in cakes find('threaded') data structure
+ * @return boolean True if this is a record array is threaded
+ */
 	public function isThreadedRecordSet() {
-		return $this->isRecordSet() && isset($this->data[0]['children']);
+		return $this->isRecordSet() && isset($this->_data[0]['children']);
 	}
 
-	/**
-	 * Returns true if the current set is in Bancha's pagination data structure
-	 *
-	 * Example:
-	 *     array(
-	 *         'count' => 100,
-	 *         'records' => array( ... find('all') structure... )
-	 *     )
-	 *
-	 * @return boolean True if this is a pagination set
-	 */
+/**
+ * Returns true if the current set is in Bancha's pagination data structure
+ *
+ * Example:
+ *     array(
+ *         'count' => 100,
+ *         'records' => array( ... find('all') structure... )
+ *     )
+ *
+ * @return boolean True if this is a pagination set
+ */
 	public function isPaginatedSet() {
-		return isset($this->data['records']) && isset($this->data['count']) &&  // this is how a paginated result set should look with Bancha
-				(isset($this->data['records']['0'][$this->primary]) || 			// paginagted records with records
-				(is_array($this->data['records']) && $this->data['count']==0)); // pagination with zero records
+		return isset($this->_data['records']) && isset($this->_data['count']) &&  // this is how a paginated result set should look with Bancha
+				(isset($this->_data['records']['0'][$this->_primary]) || 			// paginagted records with records
+				(is_array($this->_data['records']) && $this->_data['count']==0)); // pagination with zero records
 	}
 
-	/**
-	 * This function walks through the input $data and calls the given callback 
-	 * for each record found. The callback has to have two parameters:
-	 * 
-	 *  - string $modelName: The name of the model (e.g. 'User')
-	 *  - array  $data: The model data, not nested data (e.g. array('id', 'name')). 
-	 *                  If there is a hasMany or hasManyAndBelongsToMany association 
-	 *                  and there is no record, the callable function will be called 
-	 *                  once with the model name (to may transform) and null as $data.
-	 *                  Note that the record may have nested records inside the data,
-	 *                  deleting or modifing those results in the walker not 
-	 *                  visiting them.
-	 *
-	 * The callable should return a numeric array, first param is the new record
-	 * key, second the data. Setting the record key to false results in removeing
-	 * the entry.
-	 *
-	 * The walker currently does not support threaded records.
-	 * 
-	 * The walker has a depth first approach for walking the data. It has no 
-	 * side effects.
-	 * 
-	 * @param callback $callable PHP valid callback to be called for every 
-	 *                           record data found in the input data.
-	 * @return array             The resulting data array.
-	 */
+/**
+ * This function walks through the input $data and calls the given callback 
+ * for each record found. The callback has to have two parameters:
+ * 
+ *  - string $modelName: The name of the model (e.g. 'User')
+ *  - array  $data: The model data, not nested data (e.g. array('id', 'name')). 
+ *                  If there is a hasMany or hasManyAndBelongsToMany association 
+ *                  and there is no record, the callable function will be called 
+ *                  once with the model name (to may transform) and null as $data.
+ *                  Note that the record may have nested records inside the data,
+ *                  deleting or modifing those results in the walker not 
+ *                  visiting them.
+ *
+ * The callable should return a numeric array, first param is the new record
+ * key, second the data. Setting the record key to false results in removeing
+ * the entry.
+ *
+ * The walker currently does not support threaded records.
+ * 
+ * The walker has a depth first approach for walking the data. It has no 
+ * side effects.
+ * 
+ * @param callback $callable PHP valid callback to be called for every 
+ *                           record data found in the input data.
+ * @return array             The resulting data array.
+ */
 	public function walk($callable) {
 		if($this->isPaginatedSet()) {
 			// walk though the record entries only
-			$data = $this->data;
+			$data = $this->_data;
 			$data['records'] = $this->_walk($callable, $data['records']);
 			return $data;
 		} else {
 			// walk though the records
-			return $this->_walk($callable, $this->data);
+			return $this->_walk($callable, $this->_data);
 		}
 	}
 
-	private function _walk($callable, $data) {
+/**
+ * Helper function for {@link walk}
+ * @param callback $callable PHP valid callback to be called for every 
+ *                           record data found in the input data.
+ * @param  array   $data     The CakePHP data in any format
+ * @return array             The resulting data array.
+ */
+	protected function _walk($callable, $data) {
 
 		// find all data entries
 		foreach($data as $key => $value) {
@@ -154,7 +172,15 @@ class CakeSenchaDataMapper {
 		return $data;
 	}
 
-	private function _walkNestedSet($callable, $data, $modelName) {
+/**
+ * Helper function for {@link _walk}
+ * @param callback $callable  PHP valid callback to be called for every 
+ *                            record data found in the input data.
+ * @param  array   $data      The CakePHP data of a nested set (a numeric array)
+ * @param  string  $modelName The name of the models in $data
+ * @return array              The resulting data array.
+ */
+	protected function _walkNestedSet($callable, array $data, $modelName) {
 
 		// remove original entry
 		$nestedData = $data[$modelName];
