71d70dfedf9be8e6411065bc5c98d0920b2bbc82
Roland Schuetz <mail@rolandschuetz.at>
2013-12-13 10:17:25 +0100

    Code cleanup

--- Lib/Bancha/Routing/BanchaSingleDispatcher.php
+++ Lib/Bancha/Routing/BanchaSingleDispatcher.php
@@ -18,13 +18,15 @@ App::uses('Dispatcher', 'Routing');
 // to support the old before 2.2 and the new 2.2+ signature.
 if(substr(Configure::version(), 2, 3) < 2) {
 	/**
-	 * See BanchaSingleDispatcher class.
+	 * BanchaSingleDispatcher is a subclass of CakePHP's Dispatcher.
+	 *
+	 * See the method descriptions why this is required.
 	 *
 	 * @package       Bancha.Lib.Bancha.Routing
 	 * @since         Bancha v 2.0.0
 	 * @author        Roland Schuetz <mail@rolandschuetz.at>
 	 */
-	class BanchaSingleDispatcher_Parent extends Dispatcher {
+	class BanchaSingleDispatcher extends Dispatcher {
 		/**
 		 * Applies additionalParameters to the request to be dispatched. Unlike Dispatcher, BanchaSingleDispatcher does not
 		 * applies the routes.
@@ -45,13 +47,15 @@ if(substr(Configure::version(), 2, 3) < 2) {
 	}
 } else {
 	/**
-	 * See BanchaSingleDispatcher class.
+	 * BanchaSingleDispatcher is a subclass of CakePHP's Dispatcher.
 	 *
+	 * See the method descriptions why this is required.
+	 * 
 	 * @package       Bancha.Lib.Bancha.Routing
 	 * @since         Bancha v 2.0.0
 	 * @author        Roland Schuetz <mail@rolandschuetz.at>
 	 */
-	class BanchaSingleDispatcher_Parent extends Dispatcher {
+	class BanchaSingleDispatcher extends Dispatcher {
 		/**
 		 * Applies additionalParameters to the request to be dispatched. Unlike Dispatcher, BanchaSingleDispatcher does not
 		 * applies the routes.
@@ -79,59 +83,5 @@ if(substr(Configure::version(), 2, 3) < 2) {
 				$request->addParams($event->data['additionalParams']);
 			}
 		}
-	}
-}
-
-/**
- * BanchaSingleDispatcher is a subclass of CakePHP's Dispatcher.
- *
- * See the method descriptions why this is required.
- *
- * @package       Bancha.Lib.Bancha.Routing
- * @since         Bancha v 0.9.0
- * @author        Roland Schuetz <mail@rolandschuetz.at>
- * @author        Florian Eckerstorfer <f.eckerstorfer@gmail.com>
- */
-class BanchaSingleDispatcher extends BanchaSingleDispatcher_Parent {
-
-/**
-* Initializes the components and models a controller will be using.
-* Triggers the controller action, and invokes the rendering if Controller::$autoRender is true and echo's the output.
-* Otherwise the return value of the controller action are returned.
-*
-* Works like {@see Dispatcher::_invoke()} but returns the full response instead the body only.
-*
-* Bancha needs to overwrite this method because we need the full response object not only the body of the response
-* object on return.
-*
-* @param Controller $controller Controller to invoke
-* @param CakeRequest $request The request object to invoke the controller for.
-* @param CakeResponse $response The response object to receive the output
-* @return CakeResponse te resulting response object
- */
-	protected function _invoke(Controller $controller, CakeRequest $request, CakeResponse $response) {
-		$controller->constructClasses();
-		$controller->startupProcess();
-
-		$render = true;
-		$result = $controller->invokeAction($request);
-		if ($result instanceof CakeResponse) {
-			$render = false;
-			$response = $result;
-		}
-
-		if ($render && $controller->autoRender) {
-			$response = $controller->render();
-		} elseif (!($result instanceof CakeResponse) &&
-			$response->body() === null
-		) {
-			$response->body($result);
-		}
-		$controller->shutdownProcess();
-
-		if (isset($request->params['return'])) {
-			return $response; // <-------------- only this line is changed, original: return $response->body();
-		}
-		$response->send();
 	}
 }
