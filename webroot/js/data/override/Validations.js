0f0031b585dd519ffc563187f2e079c07887df61
Roland Schuetz <mail@rolandschuetz.at>
2014-08-22 19:41:52 -0500

    Compability addon

--- webroot/js/data/override/Validations.js
+++ webroot/js/data/override/Validations.js
@@ -13,6 +13,11 @@
  * For more information go to http://bancha.io
  */
 
+// Fake missing classes for production
+if(Ext.versions.extjs.major === 5) {
+    Ext.define('Ext.data.validations', {});
+}
+
 /**
  * @private
  * @class Bancha.data.override.Validations
